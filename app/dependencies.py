2d6918a33252bac03c1325c34f6353b418bfd9b5
Hansil <chapadiyahansil@gmail.com>
2026-04-21 13:51:07 +0530

    app/dependencies.py

--- app/dependencies.py
+++ app/dependencies.py
@@ -8,4 +8,4 @@ async def get_token_header(x_token: Annotated[str, Header()]):
 
 async def get_query_token(token: Annotated[str, Header()]):
     if token != os.getenv("token"):
-        raise HTTPException(status_code=400, detail="No jessica token provided")
+        raise HTTPException(status_code=400, detail="No token provided")
